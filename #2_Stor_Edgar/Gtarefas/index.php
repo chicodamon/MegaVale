af065866631e3aec0a623e5222453305a7e1bac6
chicodamon <chicodietrich@gmail.com>
2024-12-10 21:04:25 +0000

    Sing In

--- #2_Stor_Edgar/Gtarefas/index.php
+++ #2_Stor_Edgar/Gtarefas/index.php
@@ -155,26 +155,17 @@
     
 <main class="form-signin w-100 m-auto">
   <form>
-    <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
+    <h1 class="h3 mb-3 fw-normal">Bem-Vindo à área reservada</h1>
+      <p>Introduza os seus dados de login</p>
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
+      <label for="floatingInput">Email</label>
     </div>
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
-
-    <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Remember me
-      </label>
-    </div>
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+    <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
   </form>
 </main>
 <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
