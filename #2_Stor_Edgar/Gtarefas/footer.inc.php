eacb60d380709645e9a254153160f8341762541c
chicodamon <chicodietrich@gmail.com>
2024-12-12 15:39:58 +0000

    footer content e css

--- #2_Stor_Edgar/Gtarefas/footer.inc.php
+++ #2_Stor_Edgar/Gtarefas/footer.inc.php
@@ -1,8 +1,9 @@
 <html lang="en">
 <link rel="stylesheet" href="bootstrap.min.css">
+<link rel="stylesheet" href="sticky-footer.css">
 <footer class="footer mt-auto py-3 bg-body-tertiary">
   <div class="container">
-    <span class="text-body-secondary">Place sticky footer content here.</span>
+    <span class="text-body-secondary">G-Tarefas 2024-2025</span>
   </div>
 </footer>
 </html>
