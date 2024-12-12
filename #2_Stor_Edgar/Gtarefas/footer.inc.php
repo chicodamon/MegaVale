97b150c79ea886dcb452fa34bc5eb17d4619d824
chicodamon <chicodietrich@gmail.com>
2024-12-12 15:36:45 +0000

    footer

---
+++ #2_Stor_Edgar/Gtarefas/footer.inc.php
@@ -0,0 +1,8 @@
+<html lang="en">
+<link rel="stylesheet" href="bootstrap.min.css">
+<footer class="footer mt-auto py-3 bg-body-tertiary">
+  <div class="container">
+    <span class="text-body-secondary">Place sticky footer content here.</span>
+  </div>
+</footer>
+</html>
